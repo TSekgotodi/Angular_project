c21a63b3cdc9d594f453068f5b20483656681001
Thabang Sekgotodi <sekgotodit@outlook.com>
2022-10-09 01:19:19 +0200

    added personal detail card

--- TutorSystem/src/app/material/material.module.ts
+++ TutorSystem/src/app/material/material.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 const MaterialComponents = [
-  MatCardModule
+  MatCardModule,
+  MatStepperModule,
+  MatTabsModule,
+  MatFormFieldModule
 ];
 @NgModule({
   declarations: [
